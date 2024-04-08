1e184cd416538f3fe3c7ce5194822df11217ddb8
Alwin Jose <138508113+24DrkVnm@users.noreply.github.com>
2024-04-08 11:10:00 +0530

    Update and rename armstrong.c to arnstrong.c
    Added descriptions for the code

--- arnstrong.c
+++ arnstrong.c
@@ -1,8 +1,11 @@
+//Basic prograqm to check if a number is arnstorng or not .//
+//Arnstrong is a number whose sum of cubes of digits will be equal to the initial number.// 
 #include<stdio.h>
 void main(){
 int a,b=0,n,rem;
 printf("Please enter number to be checked");
 scanf("%d",&a);
+//Input is assigned to another variable to check if its arnstrong//  
 n = a;
 while(a>0)
 	{
